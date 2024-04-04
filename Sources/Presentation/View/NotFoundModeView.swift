06971dedbb3a25ac83afa0fe639a5d98300973ef
yujinkim1 <yujin7621@me.com>
2024-04-04 23:21:46 +0900

    create: Presentation 그룹

--- Sources/Presentation/View/NotFoundModeView.swift
+++ Sources/Presentation/View/NotFoundModeView.swift
@@ -1,5 +1,5 @@
 //
-//  NotFoundModeTableViewCell
+//  NotFoundModeView
 //  TimePot
 //
 //  Created by Yujin Kim on 2024-03-30.
@@ -7,29 +7,35 @@
 
 import UIKit
 
-final class NotFoundModeTableViewCell: UITableViewCell {
+final class NotFoundModeView: UIView {
     static var identifier: String {
         return NSStringFromClass(self)
     }
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "사용할 수 있는 모드가 없어요"
-        label.font = .systemFont(ofSize: CGFloat(24), weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "사용 가능한 모드가 없어요"
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: CGFloat(18), weight: .semibold)
         
         return label
     }()
     
     private lazy var subtitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "+ 버튼으로 모드를 생성하고 시간을 기록해보세요"
-        label.font = .systemFont(ofSize: CGFloat(18), weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "+ 버튼으로 새로운 모드를 생성하고 나의 시간을 기록해보세요"
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: CGFloat(11), weight: .regular)
         
         return label
     }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
         self.backgroundColor = .clear
         
@@ -43,14 +49,15 @@ final class NotFoundModeTableViewCell: UITableViewCell {
     }
     
     private func setLayouts() {
-        self.addSubviews([titleLabel, subtitleLabel])
+        self.addSubviews([self.titleLabel,
+                          self.subtitleLabel])
     }
     
     private func setConstraints() {
         self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         self.titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         
-        self.subtitleLabel.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor).isActive = true
+        self.subtitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: CGFloat(8)).isActive = true
         self.subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: CGFloat(8)).isActive = true
     }
 }

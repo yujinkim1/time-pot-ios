71fb884fb3b2555929982f6122614ee91d8d2bde
yujinkim1 <yujin7621@me.com>
2024-04-08 23:27:00 +0900

    style: 컴포넌트 프로퍼티 수정

--- Sources/Presentation/View/NotFoundModeView.swift
+++ Sources/Presentation/View/NotFoundModeView.swift
@@ -8,17 +8,13 @@
 import UIKit
 
 final class NotFoundModeView: UIView {
-    static var identifier: String {
-        return NSStringFromClass(self)
-    }
-    
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "사용 가능한 모드가 없어요"
         label.textColor = .gray
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: CGFloat(18), weight: .semibold)
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         
         return label
     }()
@@ -26,10 +22,10 @@ final class NotFoundModeView: UIView {
     private lazy var subtitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "+ 버튼으로 새로운 모드를 생성하고 나의 시간을 기록해보세요"
+        label.text = "+버튼으로 새로운 모드를 만들고 나의 시간을 기록해보세요"
         label.textColor = .gray
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: CGFloat(11), weight: .regular)
+        label.font = .systemFont(ofSize: 11, weight: .regular)
         
         return label
     }()
@@ -49,15 +45,14 @@ final class NotFoundModeView: UIView {
     }
     
     private func setLayouts() {
-        self.addSubviews([self.titleLabel,
-                          self.subtitleLabel])
+        self.addSubviews([self.titleLabel, self.subtitleLabel])
     }
     
     private func setConstraints() {
         self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         self.titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         
-        self.subtitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: CGFloat(8)).isActive = true
-        self.subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: CGFloat(8)).isActive = true
+        self.subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        self.subtitleLabel.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor).isActive = true
     }
 }

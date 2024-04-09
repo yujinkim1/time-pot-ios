596cdf9e085fea7b15a7ef817ecd282803c37d1c
yujinkim1 <yujin7621@me.com>
2024-04-09 23:13:46 +0900

    style: 메인 화면 컴포넌트 변경

--- Sources/Presentation/ViewController/MainViewController.swift
+++ Sources/Presentation/ViewController/MainViewController.swift
@@ -7,153 +7,47 @@
 
 import UIKit
 
-// TODO: 1. 셀을 동적으로 추가하도록 하기
-// TODO: 2. 데이터가 없으면 NotFoundModeTableViewCell을 보여주기
+/*
+ 1. 새로운 데이터를 동적으로 추가할 수 있도록 하기
+ 2. view = contentView
+ */
 
-final class MainViewController: BaseViewController {
-    var modeOfList: [String] = []
-    
-    lazy var modeTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.dataSource = self
-        
-        return tableView
-    }()
-    
-    private lazy var menu: UIMenu = {
-        let menu = UIMenu(children: createMenuAction())
-        
-        return menu
-    }()
-    
-    private lazy var menuButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = CGFloat(8)
-        button.setImage(UIImage(systemName: "line.3.horizontal"), for: .normal)
-        button.menu = self.menu
-        button.showsMenuAsPrimaryAction = true
-        
-        return button
-    }()
-    
-    lazy var plusButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(UIImage(systemName: "plus"), for: .normal)
-        button.backgroundColor = .gray
-        
-        return button
-    }()
-    
-    lazy var plusTouchableImageView: TouchableImageView = {
-        let imageView = TouchableImageView(frame: .zero)
-        imageView.image = UIImage(systemName: "plus")
-        
-        return imageView
-    }()
+final class MainViewController: BaseViewController<MainView> {
+    var isAddMode = false
+    var modeOfList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
         
-        self.view.backgroundColor = .white
-        
+        self.rootview.modeTableView.dataSource = self
+        self.rootview.modeTableView.delegate = self
         self.prepareModeTableView()
     }
     
-    override func setLayouts() {
-        super.setLayouts()
-        
-        self.view.addSubviews([
-            self.modeTableView,
-            self.menuButton,
-            self.plusButton
-        ])
+    override func setIfNeeds() {
+        self.rootview.plusTouchableImageView.setOpaqueTapEvent {
+            self.isAddMode = true
+            
+            self.rootview.modeTableView.beginUpdates()
+            self.rootview.modeTableView.insertRows(at: [IndexPath.init(row: 0, section: 0)], with: .automatic)
+            self.rootview.modeTableView.endUpdates()
+        }
     }
-    
-    override func setConstraints() {
-        super.setConstraints()
-        
-        self.modeTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        self.modeTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        self.modeTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        self.modeTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20).isActive = true
-        
-        self.menuButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        self.menuButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20).isActive = true
-        self.menuButton.widthAnchor.constraint(equalToConstant: CGFloat(33)).isActive = true
-        self.menuButton.heightAnchor.constraint(equalToConstant: CGFloat(33)).isActive = true
-        
-        self.plusButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        self.plusButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20).isActive = true
-        self.plusButton.widthAnchor.constraint(equalToConstant: CGFloat(33)).isActive = true
-        self.plusButton.heightAnchor.constraint(equalToConstant: CGFloat(33)).isActive = true
-    }
-    
-    func createMenuAction() -> [UIAction] {
-        var actions: [UIAction]
-        
-        let upgrade = UIAction(
-            title: "Upgrade to Pro",
-            image: UIImage(systemName: "dollarsign.circle.fill"),
-            handler: { [weak self] _ in
-                guard let selfRef = self else { return }
-                
-                let viewController = RootViewController()
-                selfRef.present(viewController, animated: true)
-            }
-        )
-        
-        let settings = UIAction(
-            title: "Settings",
-            image: UIImage(systemName: "gear.circle.fill"),
-            handler: { [weak self] _ in
-                guard let selfRef = self else { return }
-                
-                let viewController = RootViewController()
-                selfRef.present(viewController, animated: true)
-            }
-        )
-        
-        let policy = UIAction(
-            title: "Privacy policy",
-            image: UIImage(systemName: "arrow.up.right.circle.fill"),
-            handler: { [weak self] _ in
-                guard let selfRef = self else { return }
-                
-                let viewController = RootViewController()
-                selfRef.present(viewController, animated: true)
-            }
-        )
-        
-        let terms = UIAction(
-            title: "Terms of service",
-            image: UIImage(systemName: "arrow.up.right.circle.fill"),
-            handler: { [weak self] _ in
-                guard let selfRef = self else { return }
-                
-                let viewController = RootViewController()
-                selfRef.present(viewController, animated: true)
-            }
-        )
-        
-        actions = [upgrade, settings, policy, terms]
-        
-        return actions.reversed()
-    }
-    
-    @objc private func createMode(_ button: UIButton) {}
     
     private func prepareModeTableView() {
         if modeOfList.count == 0 {
-            self.modeTableView.backgroundView = NotFoundModeTableViewCell()
+            self.rootview.modeTableView.backgroundView = NotFoundModeView()
         }
+        
+        self.rootview.modeTableView.reloadData()
     }
 }
 
-extension MainViewController: UITableViewDataSource {
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
@@ -172,10 +66,14 @@ extension MainViewController: UITableViewDataSource {
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
-        return .init()
+        if indexPath.row == 0 && self.isAddMode {
+            let cell = tableView.dequeueReusableCell(withClass: NewModeCell.self)
+            
+            return cell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withClass: ModeCell.self, for: indexPath)
+        
+        return cell
     }
-}
-
-extension MainViewController: UITableViewDelegate {
-    
 }

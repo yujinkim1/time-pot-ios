a0c3480b3c797ae5f1f1c18411b5e2e958cfeb49
yujinkim1 <yujin7621@me.com>
2024-04-14 23:15:34 +0900

    style: 이니셜라이저 제거

--- Sources/Basement/BaseViewController.swift
+++ Sources/Basement/BaseViewController.swift
@@ -12,11 +12,6 @@ class BaseViewController<View: UIView>: UIViewController {
         self.view as! View
     }
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func loadView() {
         self.view = View()
     }
